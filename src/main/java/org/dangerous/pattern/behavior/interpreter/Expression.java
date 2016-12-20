5c1b27b44657564570a98744954f8e2bd122188e
dangerous1990 <dangerouslm1990@gmail.com>
2016-12-20 16:45:10 +0800

    解释器模式

---
+++ src/main/java/org/dangerous/pattern/behavior/interpreter/Expression.java
@@ -0,0 +1,8 @@
+package org.dangerous.pattern.behavior.interpreter;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+public interface Expression {
+    public boolean interpret(String context);
+}
---
+++ src/main/java/org/dangerous/pattern/behavior/interpreter/TerminalExpression.java
@@ -0,0 +1,21 @@
+package org.dangerous.pattern.behavior.interpreter;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if (context.contains(data)) {
+            return true;
+        }
+        return false;
+    }
+}
