d80ee4ae5d3a042da63af4d85523190539455078
dangerous1990 <dangerouslm1990@gmail.com>
2016-12-22 13:59:17 +0800

    组合模式

---
+++ src/main/java/org/dangerous/pattern/structure/combination/Client.java
@@ -0,0 +1,15 @@
+package org.dangerous.pattern.structure.combination;
+
+/**
+ * Created by Administrator on 2016/12/22.
+ */
+public class Client {
+    public static void main(String[] args) {
+        Tree tree = new Tree("A");
+        TreeNode nodeB = new TreeNode("B");
+        TreeNode nodeC = new TreeNode("C");
+        nodeB.add(nodeC);
+        tree.root.add(nodeB);
+        System.out.println("build the tree finished!");
+    }
+}
---
+++ src/main/java/org/dangerous/pattern/structure/combination/Tree.java
@@ -0,0 +1,13 @@
+package org.dangerous.pattern.structure.combination;
+
+public class Tree {
+
+
+    TreeNode root;
+
+    public Tree(String name) {
+        root = new TreeNode(name);
+    }
+
+
+}
