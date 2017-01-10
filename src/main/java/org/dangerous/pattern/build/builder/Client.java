32b6e1f23640ef893317e8bf2ec475c7768578a7
dangerous1990 <dangerouslm1990@gmail.com>
2017-01-10 19:43:32 +0800

    clean up

--- src/main/java/org/dangerous/pattern/build/builder/Client.java
+++ src/main/java/org/dangerous/pattern/build/builder/Client.java
@@ -1,10 +1,13 @@
 package org.dangerous.pattern.build.builder;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/12/13.
  */
 public class Client {
     public static void main(String[] args) {
+        int  a = 1_2_3;
         IncomeReportBuilder incomeReportBuilder = new IncomeReportBuilder();
         Director director = new Director(incomeReportBuilder);
         director.construct();
